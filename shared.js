c0379152639ee24d1fe0463d326c62513d6ab209
boynedmaster <boynedmaster@gmail.com>
2015-09-24 18:42:31 -0700

    Revert "Fixed everything related to isLoggedIn()"
    This reverts commit ce4b34a8080b0ea4d811cd107b54c58eb6f3c341.

--- shared.js
+++ shared.js
@@ -7,11 +7,13 @@ var notificationValues = [];
 function isLoggedIn(){
 	var ret = true;
 	
-	$.ajax({type: "POST", url: "http://www.toonbook.me/sdtopbarmenu/index/update?format=json", success: function(data){
-		ret = true;
-	}, error: function(){
-		ret = false;
-	}, async: false});
+	$.post("http://www.toonbook.me/sdtopbarmenu/index/update?format=json", function(data){
+		try{
+			JSON.parse(data);
+		}catch(e){
+			ret = false;
+		}
+	});
 	
 	return ret;
 }
@@ -63,20 +65,8 @@ function checkNotifications(){
 	}
 }
 
-function checkAll(){
-	if(!isLoggedIn()){
-		chrome.browserAction.setIcon({path: "icon_gray.png"});
-		
-		$(".logged_in").css("display", "none");
-		$(".logged_out").css("display", "block");
-	}else{
-		chrome.browserAction.setIcon({path: "icon.png"});
-		
-		$(".logged_in").css("display", "block");
-		$(".logged_out").css("display", "none");
-	}
-}
+checkNotifications();
 
-checkAll();
-
-setInterval(checkAll, 10000);
+setInterval(function(){
+	checkNotifications();
+}, 10000);
