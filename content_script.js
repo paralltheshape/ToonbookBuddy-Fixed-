5548e6d047626056f408dd4bce91c92bf0279fb2
boynedmaster <boynedmaster@gmail.com>
2015-10-25 19:57:54 -0700

    Give myself a nice nametag, ooo!

--- content_script.js
+++ content_script.js
@@ -6,33 +6,52 @@ $(document).ready(function(){
 	window.ToonbookBuddy = ToonbookBuddy;
 	
 	function emitPostEvent(post){
+		var jPost = $(post);
+		
 		ToonbookBuddy.EventEmitter.emit("post", {
-			author: $(post).find(".feed_item_username").text(),
-			message: $(post).find(".feed_item_bodytext").html(),
-			date: new Date($(post).find(".timestamp").attr("title")),
-			element: post,
+			author: jPost.find(".feed_item_username").text(),
+			authorObject: jPost.find(".feed_item_username:first"),
+			authorId: jPost.find(".feed_item_username:first").attr("rev"),
+			message: jPost.find(".feed_item_bodytext").html(),
+			messageObject: jPost.find(".feed_item_bodytext"),
+			date: new Date(jPost.find(".timestamp").attr("title")),
+			element: jPost,
 			post: true,
 			comment: false
 		});
 	}
 	
 	function emitCommentEvent(comment){
+		var jComment = $(comment);
+		
 		ToonbookBuddy.EventEmitter.emit("comment", {
-			author: $(comment).find(".comments_author").text(),
-			message: $(comment).clone().find(".comments_info").remove("div, span").text(),
-			date: new Date($(comment).find(".timestamp").attr("title")),
-			element: comment,
+			author: jComment.find(".comments_author").text(),
+			authorObject: jComment.find(".comments_author"),
+			authorId: jComment.find(".comments_author").find("a").attr("rev"),
+			message: jComment.clone().find(".comments_info").remove("div, span").text(),
+			messageObject: jComment.clone().find(".comments_info"),
+			date: new Date(jComment.find(".timestamp").attr("title")),
+			element: jComment,
 			comment: true,
 			post: false
 		});
 	}
+	
+	function addDeveloperTag(post){
+		if(post.authorId == "user_29481"){
+			post.authorObject.addClass("toon_tb_dev");
+		}
+	}
+	
+	ToonbookBuddy.EventEmitter.on("post", addDeveloperTag);
+	ToonbookBuddy.EventEmitter.on("comment", addDeveloperTag);
 	
 	$(document).arrive(".wall-action-item", function(){
 		emitPostEvent($(this));
 	});
 	
 	$(".wall-action-item").each(function(){
-		emitCommentEvent($(this));
+		emitPostEvent($(this));
 	});
 	
 	$(document).arrive(".wall-comment-item", function(){
